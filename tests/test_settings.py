fdc9b7a6f3e1657460b33b13934e4bfb84082d98
Tobi Williams <tobiwilliams003@gmail.com>
2025-09-22 23:16:12 +0100

    updated settings tests for new config structure

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -19,7 +19,8 @@ def test_load_config_success():
             "filter_criteria": {
                 "initials_column": "AJ",
                 "initials_value": "PP", 
-                "release_status_column": "AK"
+                "release_status_column": "AK",
+                "release_quantity_column": "AL"
             }
         }
     }
